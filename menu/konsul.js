2ef37308a4b66f12a43fb8ef610953bc403034b5
Nazhifah <nazpriansyah@gmail.com>
2018-10-16 22:14:08 +0700

    hm

--- menu/konsul.js
+++ menu/konsul.js
@@ -5,7 +5,7 @@ export default class Konsul extends Component {
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text>Konsul</Text>
       </View>
     )
   }
