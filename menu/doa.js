a1056620d27a73e2c6f81404fda29ef9f6d973b9
lalisa27 <43491178+lalisa27@users.noreply.github.com>
2018-10-17 15:22:14 +0700

    sum
    desc

---
+++ menu/doa.js
@@ -0,0 +1,22 @@
+export default[
+    {
+        user: 'doa1',
+        name: 'makan',
+        subject: 'judul doa makan'
+    },
+    {
+        user: 'doa2',
+        name: 'minum',
+        subject: 'judul doa minum'
+    },
+    {
+        user: 'doa3',
+        name: 'duduk',
+        subject: 'judul doa duduk'
+    },
+    {
+        user: 'doa4',
+        name: 'berdiri',
+        subject: 'judul doa berdiri'
+    }
+]
