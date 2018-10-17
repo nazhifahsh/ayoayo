12f791e5fc46ae6560441f5e36519ed0062f5791
Mufliha Safira <muflihasafira@gmail.com>
2018-10-17 11:37:58 +0700

    Update menu/menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View,TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, View,TouchableOpacity,Dimensions, ImageBackground } from 'react-native'
+import bgImage from '../src/image/Wave.jpg'
 
+const {width: WIDTH}= Dimensions.get('window')
 export default class Menu extends Component {
   render() {
     return (
+        <ImageBackground source={bgImage}
+        style={styles.backgroundContainer}>
       <View>
-          <TouchableOpacity onPress={()=>this.props.navigation.navigate('Doa')}>
+          <TouchableOpacity onPress={()=>this.props.navigation.navigate('Doa')}
+          style={styles.touch}>
         <Text>Kumpulan Doa</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>this.props.navigation.navigate('Konsul')}>
+        <TouchableOpacity onPress={()=>this.props.navigation.navigate('Konsul')}
+        style={styles.touch}>
         <Text>Konsultasi Syariah</Text>
         </TouchableOpacity>
       </View>
+      </ImageBackground>
     )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: null,
+     height:null
+  },
+  touch:{
+    width:WIDTH - 60,
+    height:45,
+    borderRadius: 25,
+    backgroundColor: '#9afdff',
+    justifyContent: 'center',
+    alignItems:'center',
+    marginTop:20
+},
+})
