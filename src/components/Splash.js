081af6906c5d7fc89299701d50f37216fd22e6c5
Mufliha Safira <muflihasafira@gmail.com>
2018-10-17 10:48:12 +0700

    Update src/components/Splash.js

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,27 +1,73 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 import {StackNavigator} from 'react-navigation'
 
+import bgImage from '../image/Greenbluegra.jpg'
+import Logo from '../image/Book3.png'
 // create a component
 export default class SplashScreen extends Component {
     render() {
-        return (
-            <View style={styles.container}>
-                <Text>Splash</Text>
-            </View>
+        return (<ImageBackground source={bgImage} style={styles.backgroundContainer}>
+           
+        
+            <View style={styles.logoContainer}>
+          <Image source={Logo} style={styles.logo}/>
+            <Text style={styles.logoText}>Doa Book</Text>
+        </View>
+        <TouchableOpacity style={styles.continue}
+          >
+            <Text>Press to continue</Text>
+        </TouchableOpacity>
+            </ImageBackground>
         );
     }
 }
 
 // define your styles
+ 
 const styles = StyleSheet.create({
-    container: {
+    backgroundContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+        width: null,
+         height:null
+      },
+      title:{
+        color:'#ffffff',
+        fontSize:50,
+        fontWeight: '500' ,
+        marginTop: 10,
+        opacity:0.5
+},
+        logo:{
+            width: 100,
+            height:  100,
+            borderRadius:20
+     },
+     logoText:{
+        color:'#ffffff',
+             fontSize:20,
+             fontWeight: '500',
+             marginTop: 10,
+             opacity:0.7
+         }, 
+           logoContainer:{
+            alignItems: 'center',
+                 marginBottom: 50
+             },
+             continue:{
+                width:WIDTH - 60,
+                height:45,
+                borderRadius: 25,
+                justifyContent: 'center',
+                alignItems:'center',
+                marginTop:20
+            },
+     
+
+
 });
 
 //make this component available to the app
