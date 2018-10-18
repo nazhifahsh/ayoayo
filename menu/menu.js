67b438c42d8ac4f7063185cb9148a95f82cb31a2
Mufliha Safira <muflihasafira@gmail.com>
2018-10-18 16:33:21 +0700

    try

--- menu/menu.js
+++ menu/menu.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react'
 import { Text, StyleSheet, View,TouchableOpacity,Dimensions,Image, ImageBackground,  } from 'react-native'
 import bgImage from '../src/image/blackg.jpg'
 import Logo from '../src/image/Book3.png'
-import {BoxShadow} from 'react-native-shadow'
+import icon1 from '../src/image/viewlist.png'
 
-
+const {width: WIDTH}= Dimensions.get('window')
 export default class Menu extends Component {
-  render = ()=> {
-    const shadowOpt = {
-      width:160,
-      height:170,
-      color:"#000",
-      border:2,
-      radius:3,
-      opacity:0.2,
-      x:0,
-      y:3,
-      style:{marginVertical:5}
-  }
+  render() {
     return (
         <ImageBackground source={bgImage}
         style={styles.backgroundContainer}>
@@ -31,19 +20,15 @@ export default class Menu extends Component {
         </View>
 
       <View style={{flexDirection:'row'}}>
-      <BoxShadow setting={shadowOpt}>
-      
                   <TouchableOpacity onPress={()=>this.props.navigation.navigate('Doa')}
           style={styles.touch}>
+          <Image source={icon1}/>
         <Text>Kumpulan Doa</Text>
         </TouchableOpacity>
-        </BoxShadow>
-        <BoxShadow setting={shadowOpt}>
         <TouchableOpacity onPress={()=>this.props.navigation.navigate('Konsul')}
         style={styles.touch2}>
         <Text>Konsultasi Syariah</Text>
         </TouchableOpacity>
-        </BoxShadow>
       </View>
       </ImageBackground>
     )
@@ -74,22 +59,19 @@ logoText:{
        opacity:0.7
    },
   touch:{
-    width:150,
-    height:90,
+    width:160,
+    height:110,
     borderRadius: 25,
     backgroundColor: '#9afdff',
     justifyContent: 'center',
     alignItems:'center',
     marginRight:20,
-    shadowColor:'#2db7fd',
-    shadowOpacity:0.5,
-    shadowOffset:{width:170, height:110},
     marginLeft:20,
     opacity:0.8
 },
 touch2:{
-  width:150,
-  height:90,
+  width:160,
+  height:110,
   borderRadius: 25,
   backgroundColor: '#7192ff',
   justifyContent: 'center',
