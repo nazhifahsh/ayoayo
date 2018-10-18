fbeb72a0c3376ac89299b9aded45bd64269668dc
elhaa <nazhifahshofap@gmail.com>
2018-10-18 22:45:18 +0700

    icon and list Q&A

--- snr/routes.js
+++ snr/routes.js
@@ -3,6 +3,8 @@ import Splash from './splash';
 import Menu from '../menu/menu';
 import Doa from '../menu/indexdoa';
 import Konsul from '../menu/konsul';
+import QandA from '../menu/indexQ&A'; 
+import LMB from '../menu/kumpulananswer/laranganmakanberdiri';
 import UstAulia from '../menu/contactperson/ustadzaulia';
 import UstBurhan from '../menu/contactperson/ustadzaulia';
 import UstFauzan from '../menu/contactperson/ustadzaulia';
@@ -13,6 +15,8 @@ const Routes = StackNavigator({
     Menu: { screen: Menu, navigationOptions:{header:null} },
     Doa: { screen: Doa, navigationOptions:{header:null} },
     Konsul:{screen: Konsul, navigationOptions:{header:null} },
+    QandA:{screen: QandA,navigationOptions:{header:null}},
+    LMB:{screen: LMB, navigationOptions:{header:null}},
     UstAulia:{screen: UstAulia, navigationOptions:{header:null}},
     UstBurhan:{screen: UstBurhan, navigationOptions:{header:null}},
     UstFauzan:{screen: UstFauzan, navigationOptions:{header:null}},
