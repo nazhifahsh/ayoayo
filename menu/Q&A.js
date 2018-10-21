dba64c20bd93baf450082b35e837ad7150694890
Mufliha Safira <muflihasafira@gmail.com>
2018-10-21 09:18:11 +0700

    Update menu/Q&A.js

--- menu/Q&A.js
+++ menu/Q&A.js
@@ -1,22 +1,28 @@
-export default class QnA  {[
-    {
-        user: 'Q1',
-        name: 'makan berdiri',
-        subject: 'Larangan makan berdiri'
-    },
-    {
-        user: 'Q2',
-        name: 'minum berdiri',
-        subject: 'Larangan minum berdiri'
-    },
-    {
-        user: 'Q3',
-        name: 'sebelum tidur',
-        subject: 'Sunnah Rasul sebelum tidur'
-    },
-    {
-        user: 'Q4',
-        name: 'setelah tidur',
-        subject: 'Sunnah Rasul setelah tidur'
+export default class QnA  {
+    constructor(props){
+        super(props);
+        this.state = {
+            index:[{
+                user: 'Q1',
+                name: 'makan berdiri',
+                subject: 'Larangan makan berdiri'
+            }]
+        },
+        {
+            user: 'Q2',
+            name: 'minum berdiri',
+            subject: 'Larangan minum berdiri'
+        },
+        {
+            user: 'Q3',
+            name: 'sebelum tidur',
+            subject: 'Sunnah Rasul sebelum tidur'
+        },
+        {
+            user: 'Q4',
+            name: 'setelah tidur',
+            subject: 'Sunnah Rasul setelah tidur'
+        }
+            
+        }
     }
-]}
