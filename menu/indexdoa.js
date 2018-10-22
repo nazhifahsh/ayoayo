7f22bcaf4160c43fdcf31eeaac01d30dd690f1dd
Nazhifah <nazpriansyah@gmail.com>
2018-10-22 09:24:29 +0700

    h

--- menu/indexdoa.js
+++ menu/indexdoa.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import SearchInput, { createFilter } from 'react-native-search-filter';
-<<<<<<< HEAD
 import doa from './doa';
 const cari = ['user.name', 'subject'];
-=======
-import emails from './doa';
-import icon from '../src/image/Cari.png'
-import {Icon} from 'react-native-vector-icons'
-import bgImage from '../src/image/deermount.jpg'
-const KEYS_TO_FILTERS = ['user.name', 'subject'];
->>>>>>> 5ed93dede35c04b23fe388fe71415e6990200e97
  
 export default class App extends Component {
  constructor(props) {
@@ -32,13 +24,8 @@ export default class App extends Component {
         <SearchInput 
           onChangeText={(term) => { this.searchUpdated(term) }} 
           style={styles.searchInput}
-<<<<<<< HEAD
           placeholder="Type here to search"
           />
-=======
-          placeholder='Search Doa'
-        />
->>>>>>> 5ed93dede35c04b23fe388fe71415e6990200e97
         <ScrollView>
           {filteredEmails.map(doa => {
             return (
