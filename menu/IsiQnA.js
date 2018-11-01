e0ac2af0ede4a1b0c6855dc10f2e49d5d5c9b264
elhaa <nazhifahshofap@gmail.com>
2018-11-01 15:06:05 +0700

    Update menu/IsiQnA.js

--- menu/IsiQnA.js
+++ menu/IsiQnA.js
@@ -5,4 +5,59 @@ export default[
         isi:'Penjelasan Lapang Dada menurut Ust.Hanan Attaki',
         subject: require('../src/video/1.mp4')
     },
+    {
+        user:'doa2',
+        name:'Apakah Didalam kesulitan ada kemudahan(4)?',
+        isi:'Penjelasan Didalam Kesulitan Ada Kemudahan menurut Ust.Hanan Attaki Part.4',
+        subject: require('../src/video/2.mp4')
+    },
+    {
+        user:'doa3',
+        name:'Apakah Didalam kesulitan ada kemudahan(5)?',
+        isi:'Penjelasan Didalam Kesulitan Ada Kemudahan menurut Ust.Hanan Attaki Part.5',
+        subject: require('../src/video/3.mp4')
+    },
+    {
+        user:'doa4',
+        name:'Apakah kita harus evaluasi diri(3)?',
+        isi:'Penjelasan Evaluasi menurut Ust.Hanan Attaki Part.3',
+        subject: require('../src/video/4.mp4')
+    },
+    {
+        user:'doa5',
+        name:'Apakah kita harus evaluasi diri(4)?',
+        isi:'Penjelasan Evaluasi menurut Ust.Hanan Attaki Part.4',
+        subject: require('../src/video/5.mp4')
+    },
+    {
+        user:'doa6',
+        name:'Apakah kita diharuskan untuk memaaafkan(2)?',
+        isi:'Penjelasan Memaafkan menurut Ust.Hanan Attaki Part.2',
+        subject: require('../src/video/6.mp4')
+    },
+    {
+        user:'doa7',
+        name:'Apa Tanggapan terbaik ketika diberi Musibah?',
+        isi:'Penjelasan Musibah menurut Ust.Hanan Attaki',
+        subject: require('../src/video/7.mp4')
+    },
+    {
+        user:'doa8',
+        name:'Apa yang membuat kita riya?',
+        isi:'Penjelasan Riya menurut Ust.Hanan Attaki',
+        subject: require('../src/video/8.mp4')
+    },
+    {
+        user:'doa9',
+        name:'Kenapa sering merasa punya banyak masalah?',
+        isi:'Penjelasan Solusi Masalah menurut Ust.Hanan Attaki',
+        subject: require('../src/video/9.mp4')
+    },
+    {
+        user:'doa10',
+        name:'Apa yang kita lakukan jika Teman punya masalah?',
+        isi:'Penjelasan Tindakan yang dilakukan ketika Teman punya masalah menurut Ust.Hanan Attaki',
+        subject: require('../src/video/10.mp4')
+    },
+    
 ]
